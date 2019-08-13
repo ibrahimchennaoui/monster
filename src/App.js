54cf5f35b9acb0647953e455b4b59bc678e5d6a6
ibrahima00 <37820803+ibrahima00@users.noreply.github.com>
2019-08-13 19:38:09 +0200

    commit  -m new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => this.setState({ monsters: data }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        {this.state.monsters.map(el =>
+          (<ul key={el.id} >
+            <h2>  {el.email} :  {el.name}</h2>
+          </ul>
+        ))}
+
+      </div>
+    );
+  }
 }
 
 export default App;
